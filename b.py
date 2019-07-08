69496772177d1f80efe82cfd79fff26dc89c9799
shaynelyj <47095119+shaynelyj@users.noreply.github.com>
2019-07-08 14:19:15 +0800

    Add files via upload

---
+++ b.py
@@ -0,0 +1,13 @@
+from pwn import *
+q='abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_{@}$!"#%&()*+,-./:;<=>?[]^`|~'
+k=""
+for z in range(48):
+	for _ in q:
+		p=remote("2018shell.picoctf.com",30399)
+		prompt=p.readuntil('situation report: ')
+		p.sendline("x"*(59-len(k))+"fying code is: "+k+_+"Y"*(48-len(k)))
+		e=p.read(4096)
+		if e[224:256]==e[384:416]:
+			k+=_
+			print("Flag: %s" %(k))
+			break
