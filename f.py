2697b3a2b5a7f1361c057e2ef627fb76edf4a515
shaynelyj <47095119+shaynelyj@users.noreply.github.com>
2019-07-15 14:32:13 +0800

    Add files via upload

---
+++ f.py
@@ -0,0 +1,29 @@
+from pwn import *
+from paddingoracle import BadPaddingException, PaddingOracle
+import json
+from Crypto.Cipher import AES
+
+class PadBuster(PaddingOracle):
+    def oracle(self, data):
+        while True:
+            try:
+                r = remote("2018shell.picoctf.com", 27533)
+                r.recvuntil("cookie?")
+                s = data
+                s = str(data).encode("hex")
+                r.sendline(s)
+                out = r.recvall()
+                if "invalid padding" in out:
+                    raise BadPaddingException
+                return
+            except (socket.error, socket.gaierror, socket.herror, socket.timeout) as e:
+                print str(e)
+
+if __name__ == '__main__':
+    d = {"username": "User", "is_admin": "true", "expires": "2019-10-01"}
+    s = json.dumps(d)
+    print s
+    padbuster = PadBuster()
+    encrypted = padbuster.encrypt(s, block_size=AES.block_size, iv="This is an IV456")
+
+    print "Ciphertext: %r" % (str(encrypted).encode("hex"))
