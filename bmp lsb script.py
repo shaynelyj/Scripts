6678cef64f91a7143a561761bb4b2a523b2e626a
shaynelyj <47095119+shaynelyj@users.noreply.github.com>
2019-07-19 12:03:42 +0800

    Update bmp lsb script.py

--- bmp lsb script.py
+++ bmp lsb script.py
@@ -2,18 +2,13 @@ import binascii
 
 image = open("pico2018-special-logo.bmp","rb").read()
 image=image[54:]
-#q=0
-answer=''
-s=''
+s=""
+a=""
 for c in image:
-	print("c: %s"%(c))
-	s += chr(ord('0')+(c & 1))
-	print("s: %s"%(s))
+	s+=str(c)
 	if len(s)==8:
 		if s == "00000000":
 			break
-		#print(int(s,2))
-		answer+=chr(int(s,2))
-		print(answer)
-		s=''
-		#q+=1
+		a+=(chr(int(s,2)))
+		print(a)
+		s=""
